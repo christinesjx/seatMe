6db1563c6ae7f5b6fb1b63ac0ac8bc2cd13681cb
ytpan <406008843@qq.com>
2020-02-24 19:51:00 -0500

    Header calls ClientBusinessDropdown

---
+++ Component/customerSide/Header/ClientBusinessDropdown.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class ClientBusinessDropdown extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showMenu: false,
+        }
+
+        this.showMenu = this.showMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+    }
+
+    showMenu(event) {
+        event.preventDefault();
+
+        this.setState({ showMenu: true }, () => {
+            document.addEventListener('click', this.closeMenu);
+        });
+    }
+
+    closeMenu(event) {
+        if (!this.dropdownMenu.contains(event.target)) {
+            this.setState({ showMenu: false}, () => {
+                document.removeEventListener('click', this.closeMenu);
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="dropdown">
+                <button className="dropdown-button" onClick={this.showMenu}>
+                    Menu
+                </button>
+
+                {this.state.showMenu ? 
+                    (
+                    <div
+                        ref={(element) => {
+                            this.dropdownMenu = element;
+                        }}            
+                    >
+                        <div className="dropdown-item">
+                            <button>Client</button>
+                            <button>Business</button> 
+                        </div>  
+                    </div>
+                    )
+                    :(
+                        null
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+export default ClientBusinessDropdown;
---
+++ Component/customerSide/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ClientBusinessDropdown from './ClientBusinessDropdown'
+
+const Header = () => {
+    return(
+        <div>
+            <h2>SeatMe</h2>
+            <ClientBusinessDropdown />
+        </div>
+    )
+}
+
+export default Header
